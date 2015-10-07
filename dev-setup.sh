c8d6d440122e6edf885f01077f054946686e12c6
gngeorgiev <gngeorgiev.it@gmail.com>
2015-10-07 18:42:03 +0300

    Added dev-setup script

---
+++ dev-setup.sh
@@ -0,0 +1,11 @@
+docker -v
+
+docker pull apcera/gnatsd
+docker pull mongo:latest
+
+docker run --name nats -p 4222:4222 -p 8333:8333 -d apcera/gnatsd:latest -m 8333
+docker run --name mongodb -p 27017:27017 -v /data/mongo:/data/db -d mongo:latest --storageEngine wiredTiger
+
+docker ps
+
+echo "All done!"
